0d1a7337d23e656bfb0eac19ac496e615c829669
Mikhail Bashkirov <bashmish@gmail.com>
2018-11-09 01:48:16 +0100

    test(lit-css): rename test cases for consistency and clarity

--- packages/lit-css/test/lit-css.spec.js
+++ packages/lit-css/test/lit-css.spec.js
@@ -2,25 +2,25 @@ import { expect } from 'chai';
 import css from '../src/lit-css.js';
 
 describe('lit-css', () => {
-  it('should coerce to string containing same CSS', () => {
+  it('is automatically coerced to a string containing the same CSS', () => {
     const style = css`.c{}`;
     expect(`${style}`).to.equal('.c{}');
   });
 
-  it('should compose with other styles', () => {
+  it('allows composition of multiple style modules', () => {
     const s1 = css`.c1{}`;
     const s2 = css`.c2{}`;
-    expect(css`${s1}${s2}`.toString()).to.equal('.c1{}.c2{}');
+    expect(css`${s1}${s2}.c3{}`.toString()).to.equal('.c1{}.c2{}.c3{}');
   });
 
   describe('deduping', () => {
-    it('should dedupe same style applied to the same template literal', () => {
+    it('dedupes same style modules applied to the same template literal', () => {
       const s1 = css`.c1{}`;
       const s2 = css`.c2{}`;
       expect(css`${s1}${s2}${s1}`.toString()).to.equal('.c1{}.c2{}');
     });
 
-    it('should dedupe nested styles which are parts of the applied styles', () => {
+    it('dedupes nested style modules which are parts of the applied styles', () => {
       const s1 = css`.c1{}`;
       const s2 = css`.c2{}`;
       const s3 = css`.c3{}`;
@@ -29,7 +29,7 @@ describe('lit-css', () => {
       expect(css`${s1_2}${s2_3}`.toString()).to.equal('.c1{}.c2{}.c3{}');
     });
 
-    it('should dedupe styles on any level up until the same module has been applied before', () => {
+    it('dedupes style modules on any level up until the same module has been applied before', () => {
       const s1 = css`.c1{}`;
       const s2_1 = css`${s1}.c2{}`;
       const s2_2 = css`.c2{}${s1}`;
@@ -39,7 +39,7 @@ describe('lit-css', () => {
       expect(css`${s2_2}${s1}`.toString()).to.equal('.c2{}.c1{}');
     });
 
-    it('should not dedupe other objects besides the ones created with the literal', () => {
+    it('does not dedupe other objects besides the ones created with the literal', () => {
       const clr = 'red';
       const size = 1;
       const s1 = css`.c1{color:${clr};font-size:${size}px;}`;
